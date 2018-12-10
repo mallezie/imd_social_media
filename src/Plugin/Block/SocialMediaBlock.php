14943727b213bab20796f35a51dfc4e3691374da
Tim Mallezie <tim.mallezie@gmail.com>
2018-12-10 11:11:54 +0100

    Add block structure

---
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\imd_social_media\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Defines a social menu block.
+ *
+ * @Block(
+ *  id = "imd_social_media_block",
+ *  admin_label = @Translation("Social media"),
+ * )
+ */
+class SocialMediaBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+      return [
+          '#markup' => $this->t("Empty block"),
+      ];
+  }
+
+}
