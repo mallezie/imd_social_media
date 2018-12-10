fb2a9a833f7fd853b860738d71c01a9456bbded7
Tim Mallezie <tim.mallezie@gmail.com>
2018-12-10 12:56:33 +0100

    Dependency injection for the state object

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -4,9 +4,36 @@ namespace Drupal\imd_social_media\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SettingsForm extends FormBase
 {
+    /**
+     * The state.
+     *
+     * @var \Drupal\Core\State\StateInterface
+     */
+    protected $state;
+
+    /**
+     * Constructor.
+     *
+     * @param \Drupal\Core\State\StateInterface $state
+     *   The state.
+     */
+    public function __construct(StateInterface $state) {
+        $this->state = $state;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('state')
+        );
+    }
 
     public function getFormId()
     {
@@ -18,22 +45,22 @@ class SettingsForm extends FormBase
         $form['facebook_url'] = [
             '#type' => 'url',
             '#title' => 'Facebook URL',
-            '#default_value' => \Drupal::state()->get('imd_social_media.facebook_url'),
+            '#default_value' => $this->state->get('imd_social_media.facebook_url'),
         ];
         $form['twitter_url'] = [
             '#type' => 'url',
             '#title' => 'Twitter URL',
-            '#default_value' => \Drupal::state()->get('imd_social_media.twitter_url'),
+            '#default_value' => $this->state->get('imd_social_media.twitter_url'),
         ];
         $form['linkedin_url'] = [
             '#type' => 'url',
             '#title' => 'Linkedin URL',
-            '#default_value' => \Drupal::state()->get('imd_social_media.linkedin_url'),
+            '#default_value' => $this->state->get('imd_social_media.linkedin_url'),
         ];
         $form['instagram_url'] = [
             '#type' => 'url',
             '#title' => 'Instagram URL',
-            '#default_value' => \Drupal::state()->get('imd_social_media.instagram_url'),
+            '#default_value' => $this->state->get('imd_social_media.instagram_url'),
         ];
         $form['submit'] = [
             '#type' => 'submit',
@@ -46,10 +73,10 @@ class SettingsForm extends FormBase
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        \Drupal::state()->set('imd_social_media.facebook_url', $form_state->getValue('facebook_url'));
-        \Drupal::state()->set('imd_social_media.twitter_url', $form_state->getValue('twitter_url'));
-        \Drupal::state()->set('imd_social_media.linkedin_url', $form_state->getValue('linkedin_url'));
-        \Drupal::state()->set('imd_social_media.instagram_url', $form_state->getValue('instagram_url'));
+        $this->state->set('imd_social_media.facebook_url', $form_state->getValue('facebook_url'));
+        $this->state->set('imd_social_media.twitter_url', $form_state->getValue('twitter_url'));
+        $this->state->set('imd_social_media.linkedin_url', $form_state->getValue('linkedin_url'));
+        $this->state->set('imd_social_media.instagram_url', $form_state->getValue('instagram_url'));
         \Drupal::messenger()->addMessage('De Social media links zijn succesvol opgeslagen');
     }
 }
--- src/Plugin/Block/SocialMediaBlock.php
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\imd_social_media\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a social menu block.
@@ -12,7 +15,43 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Social media"),
  * )
  */
-class SocialMediaBlock extends BlockBase {
+class SocialMediaBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+    /**
+     * The state.
+     *
+     * @var \Drupal\Core\State\StateInterface
+     */
+    protected $state;
+
+    /**
+     * Constructor.
+     *
+     * @param array $configuration
+     *   A configuration array containing information about the plugin instance.
+     * @param string $plugin_id
+     *   The plugin_id for the plugin instance.
+     * @param mixed $plugin_definition
+     *   The plugin implementation definition.
+     * @param \Drupal\Core\State\StateInterface $state
+     *   The state.
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state) {
+        parent::__construct($configuration, $plugin_id, $plugin_definition);
+        $this->state = $state;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->get('state')
+        );
+    }
 
     /**
      * {@inheritdoc}
@@ -20,10 +59,10 @@ class SocialMediaBlock extends BlockBase {
     public function build() {
         return [
             '#theme' => 'social-media',
-            '#facebook_url' => \Drupal::state()->get('imd_social_media.facebook_url'),
-            '#twitter_url' => \Drupal::state()->get('imd_social_media.twitter_url'),
-            '#linkedin_url' => \Drupal::state()->get('imd_social_media.linkedin_url'),
-            '#instagram_url' => \Drupal::state()->get('imd_social_media.instagram_url'),
+            '#facebook_url' => $this->state->get('imd_social_media.facebook_url'),
+            '#twitter_url' => $this->state->get('imd_social_media.twitter_url'),
+            '#linkedin_url' => $this->state->get('imd_social_media.linkedin_url'),
+            '#instagram_url' => $this->state->get('imd_social_media.instagram_url'),
         ];
     }
 
